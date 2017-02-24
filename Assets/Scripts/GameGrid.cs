cff39384a75f39879a68bd04aadcf0f79b7606f9
Joshua Randall <josh.randall16@gmail.com>
2017-02-23 21:50:03 -0500

    LockPickingGame
    -Startup Files of LockPicking Game

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGrid : MonoBehaviour {
+
+	//Declared Variables
+	//Reference to variables will be here
+	//and they will be public
+
+
+	
+
+
+	//Create/Generate a Grid 
+	//Reference to grid here which will be an
+	//Two dimensional Array
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	//Enumerator here that will consist of 
+	// Player, Block, Empty grid Spot, and etc
+	//....Code....
+
+
+
+	//Switch Case Statement for conditions for the player
+	//....Code....
+
+
+
+
+
+
+
+
+
+	//public functions will go down here..
+	//....Code....
+
+}
