04312cded1729e51925c547e4fef8e57ca8168e7
Joshua Randall <josh.randall16@gmail.com>
2017-02-25 00:33:04 -0500

    Created Assets, and script
    -Created a player cube, block cube, and a Player Script.

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -6,26 +6,48 @@ using UnityEngine.UI;
 public class GameGrid : MonoBehaviour {
 
 	//Declared Variables
-	//Reference to variables will be here
-	//and they will be public
 
+	public static GameGrid instance;
+	public GameObject TilePrefab;
 
-	
+	public  GameObject player;
+	public GameObject block;
+
+	public Transform playerSpawner;
+	public Transform newPos;
+
 
 
 	//Create/Generate a Grid 
-	//Reference to grid here which will be an
-	//Two dimensional Array
+	public GameObject [,] blockTiles; //Reference to grid here which will be an
+
+	//Map Size
+	public Vector2 m_mapSize;
+
+	//Outline range for each tile
+	[Range(0,1)]
+	public float m_outlinePercent;
+
+
+
+	void Awake()
+	{
+		instance = this;
+	}
 
 
 
 
-
-
-
-	// Use this for initialization
 	void Start () {
-		
+
+		createBlocks (0,0,0);
+		Instantiate(player, playerSpawner);
+
+		//Reference to the enum
+
+		//Reference to the generateMap function
+		generateMap ();
+
 	}
 	
 	// Update is called once per frame
@@ -37,7 +59,17 @@ public class GameGrid : MonoBehaviour {
 
 	//Enumerator here that will consist of 
 	// Player, Block, Empty grid Spot, and etc
-	//....Code....
+
+	public enum WorldObjects
+	{
+		PLAYER,
+		EMPTYGRID,
+		BLOCKS
+
+	}
+
+
+
 
 
 
@@ -48,11 +80,51 @@ public class GameGrid : MonoBehaviour {
 
 
 
-
-
-
-
 	//public functions will go down here..
 	//....Code....
+
+
+
+	public void generateMap()
+	{
+		
+		blockTiles = new GameObject[12,12];
+
+
+		//This is for generating tiles on the "X" axis
+		for (int x = 0; x < m_mapSize.x; x++) {
+
+
+			//This is for generating tiles on the "Y" axis
+			for (int y = 0; y < m_mapSize.y; y++) {
+
+				Vector3 m_tilePosition = new Vector3 (-m_mapSize.x / 2 + 1.0f + x, 
+					                         -m_mapSize.y / 2 + 1.0f + y,0);
+			
+			
+				GameObject go = GameObject.Instantiate (TilePrefab, m_tilePosition,
+					                Quaternion.Euler (Vector3.zero)) as GameObject;
+			
+				blockTiles [x, y] = go;
+			
+			
+
+			}
+
+
+
+		}
+			
+
+	}
+
+
+	public void createBlocks(int x, int y,int z)
+	{
+		Instantiate (block, new Vector4 (x - 1, 0, -1.09f,0f), new Quaternion (0, 0, 0, 0));
+
+	}
+
+
 
 }
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+
+	GameGrid gamegrid; //Reference to gamegrid script
+	Transform newPos;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		characterController();
+	}
+
+
+
+	//Insert Character gameobject in world
+	//Also take empty boxes?
+	public void characterController()
+	{
+		if (Input.GetKeyDown(KeyCode.A)) {
+
+
+			transform.Translate (-1, 0, 0 * Time.deltaTime) ;
+			Debug.Log ("You moved left");
+			//transform.Translate = gamegrid.blockTiles[x + 1];
+			//gamegrid.blockTiles [x - 1, y];
+
+
+
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.D)) {
+
+			transform.Translate (1, 0, 0 * Time.deltaTime);
+		}
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+
+			transform.Translate (0, 1, 0 * Time.deltaTime);
+		}
+
+		if (Input.GetKeyDown(KeyCode.S)) {
+
+			transform.Translate (0, -1, 0 * Time.deltaTime);
+		}
+
+
+
+	}
+
+
+
+	//Colliding with gameobjects
+
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.tag == ("Block"))
+			{
+
+
+			}
+
+	}
+
+	
+
+}
